1235254ccaef4bac20a92b9b789606eee5ebd8db
1喃遗 <yyyllllming1001@gmail.com>
2026-03-07 14:30:31 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
